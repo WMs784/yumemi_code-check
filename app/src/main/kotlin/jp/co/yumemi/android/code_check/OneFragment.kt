3cee6ac07ebeb5bb1dfb277ba24eeb2409a595ed
WMs784 <wms784.app@gmail.com>
2022-04-18 11:24:30 +0900

    Enabled to run the app

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
@@ -22,11 +22,11 @@ class OneFragment: Fragment(R.layout.fragment_one){
 
         val _binding= FragmentOneBinding.bind(view)
 
-        val _viewModel= OneViewModel(context!!)
+        val _viewModel= context?.let { OneViewModel(it) }
 
-        val _layoutManager= LinearLayoutManager(context!!)
+        val _layoutManager= LinearLayoutManager(context)
         val _dividerItemDecoration=
-            DividerItemDecoration(context!!, _layoutManager.orientation)
+            DividerItemDecoration(context, _layoutManager.orientation)
         val _adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
             override fun itemClick(item: item){
                 gotoRepositoryFragment(item)
@@ -37,7 +37,7 @@ class OneFragment: Fragment(R.layout.fragment_one){
             .setOnEditorActionListener{ editText, action, _ ->
                 if (action== EditorInfo.IME_ACTION_SEARCH){
                     editText.text.toString().let {
-                        _viewModel.searchResults(it).apply{
+                        _viewModel?.searchResults(it).apply{
                             _adapter.submitList(this)
                         }
                     }
