0ad02ba0a6a6050b913733c18193bee1b318c868
WMs784 <wms784.app@gmail.com>
2022-04-22 10:09:04 +0900

    Renamed Item class to item

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -27,7 +27,7 @@ class OneViewModel(
 ) : ViewModel() {
 
     // 検索結果
-    fun searchResults(inputText: String): List<Item> = runBlocking {
+    fun searchResults(inputText: String): List<item> = runBlocking {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
@@ -40,7 +40,7 @@ class OneViewModel(
 
             val jsonItems = jsonBody.optJSONArray("items")
 
-            val items = mutableListOf<Item>()
+            val items = mutableListOf<item>()
 
             /**
              * アイテムの個数分ループする
@@ -56,7 +56,7 @@ class OneViewModel(
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
                 items.add(
-                    Item(
+                    item(
                         name = name,
                         ownerIconUrl = ownerIconUrl,
                         language = context.getString(R.string.written_language, language),
@@ -76,7 +76,7 @@ class OneViewModel(
 }
 
 @Parcelize
-data class Item(
+data class item(
     val name: String?,
     val ownerIconUrl: String?,
     val language: String?,
