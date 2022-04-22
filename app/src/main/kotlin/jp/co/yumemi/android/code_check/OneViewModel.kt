ab222420d58a4f16c148af61aec1f624c57b74fb
WMs784 <wms784.app@gmail.com>
2022-04-22 09:21:11 +0900

    deleted !!

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -38,7 +38,7 @@ class OneViewModel(
 
             val jsonBody = JSONObject(response.receive<String>())
 
-            val jsonItems = jsonBody.optJSONArray("items")!!
+            val jsonItems = jsonBody.optJSONArray("items")
 
             val items = mutableListOf<item>()
 
@@ -46,9 +46,9 @@ class OneViewModel(
              * アイテムの個数分ループする
              */
             for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)!!
+                val jsonItem = jsonItems.optJSONObject(i)
                 val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
+                val ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url")
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
@@ -77,11 +77,11 @@ class OneViewModel(
 
 @Parcelize
 data class item(
-    val name: String,
-    val ownerIconUrl: String,
-    val language: String,
-    val stargazersCount: Long,
-    val watchersCount: Long,
-    val forksCount: Long,
-    val openIssuesCount: Long,
+    val name: String?,
+    val ownerIconUrl: String?,
+    val language: String?,
+    val stargazersCount: Long?,
+    val watchersCount: Long?,
+    val forksCount: Long?,
+    val openIssuesCount: Long?,
 ) : Parcelable
