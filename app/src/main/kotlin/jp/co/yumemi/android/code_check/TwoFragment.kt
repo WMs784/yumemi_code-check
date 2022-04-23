c6fb32e9ef5b59d3d7a8327631ee65b02ed9d935
WMs784 <wms784.app@gmail.com>
2022-04-23 10:35:15 +0900

    Added destroy function

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
@@ -3,6 +3,7 @@
  */
 package jp.co.yumemi.android.code_check
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -28,12 +29,17 @@ class TwoFragment : Fragment(R.layout.fragment_two) {
 
         var item = args.item
 
-        _binding?.ownerIconView?.load(item.ownerIconUrl);
-        _binding?.nameView?.text = item.name;
-        _binding?.languageView?.text = item.language;
-        _binding?.starsView?.text = "${item.stargazersCount} stars";
-        _binding?.watchersView?.text = "${item.watchersCount} watchers";
-        _binding?.forksView?.text = "${item.forksCount} forks";
-        _binding?.openIssuesView?.text = "${item.openIssuesCount} open issues";
+        _binding?.ownerIconView?.load(item.ownerIconUrl)
+        _binding?.nameView?.text = item.name
+        _binding?.languageView?.text = item.language
+        _binding?.starsView?.text = "${item.stargazersCount} stars"
+        _binding?.watchersView?.text = "${item.watchersCount} watchers"
+        _binding?.forksView?.text = "${item.forksCount} forks"
+        _binding?.openIssuesView?.text = "${item.openIssuesCount} open issues"
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
     }
 }
